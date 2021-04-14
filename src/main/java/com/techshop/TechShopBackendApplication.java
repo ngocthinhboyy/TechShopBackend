252e907f23515cb66cdf2411d217af1fc1efa1b1
ngocthinhboyy <ngocthinh1c1@gmail.com>
2021-04-14 11:34:59 +0700

    Create and config for Tech shop backend project

---
+++ src/main/java/com/techshop/TechShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.techshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechShopBackendApplication.class, args);
+	}
+
+}
